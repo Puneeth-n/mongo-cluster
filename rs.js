68172ea130148341cdcbc1b9a7509ce7e095ff9c
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2018-04-17 14:29:40 +0200

    init

---
+++ rs.js
@@ -0,0 +1,9 @@
+rs.initiate({
+  "_id": "rs0",
+  "version": 1,
+  "members" : [
+   {"_id": 1, "host": "db01:27017"},
+   {"_id": 2, "host": "db02:27017"},
+   {"_id": 3, "host": "db03:27017"}
+  ]
+})
