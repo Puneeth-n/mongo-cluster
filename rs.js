959ca900e264eaad388a9758c4ace3ebc0810a7c
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2018-04-18 15:32:33 +0200

    add single node config

--- rs.js
+++ rs.js
@@ -2,8 +2,8 @@ rs.initiate({
   "_id": "rs0",
   "version": 1,
   "members" : [
-   {"_id": 1, "host": "db01:27017"},
-   {"_id": 2, "host": "db02:27017"},
-   {"_id": 3, "host": "db03:27017"}
+   {"_id": 1, "host": "db1.comtravo.com:30001"},
+   {"_id": 2, "host": "db2.comtravo.com:30002"},
+   {"_id": 3, "host": "db3.comtravo.com:30003"}
   ]
 })
